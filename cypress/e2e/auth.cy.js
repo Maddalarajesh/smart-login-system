614ad681b9f925b1ca1db4e2d5a35f6ca30ff86e
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-05-14 11:50:44 +0530

    Create auth.cy.js

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,55 @@
+describe("Smart Login System", () => {
+  beforeEach(() => {
+    cy.clearLocalStorage();
+  });
+
+  it("should signup successfully", () => {
+    cy.visit("/signup.html");
+
+    cy.get("#signupName").type("Rajesh");
+    cy.get("#signupEmail").type("rajesh@gmail.com");
+    cy.get("#signupPassword").type("123456");
+
+    cy.get("#signupBtn").click();
+
+    cy.window().then((win) => {
+      const users = JSON.parse(win.localStorage.getItem("users"));
+
+      expect(users[0].email).to.equal("rajesh@gmail.com");
+    });
+  });
+
+  it("should login successfully", () => {
+    const users = [
+      {
+        name: "Rajesh",
+        email: "rajesh@gmail.com",
+        password: "123456",
+      },
+    ];
+
+    cy.visit("/index.html");
+
+    cy.window().then((win) => {
+      win.localStorage.setItem("users", JSON.stringify(users));
+    });
+
+    cy.get("#loginEmail").type("rajesh@gmail.com");
+    cy.get("#loginPassword").type("123456");
+
+    cy.get("#loginBtn").click();
+
+    cy.url().should("include", "home.html");
+  });
+
+  it("should fail with invalid credentials", () => {
+    cy.visit("/index.html");
+
+    cy.get("#loginEmail").type("wrong@gmail.com");
+    cy.get("#loginPassword").type("wrongpassword");
+
+    cy.get("#loginBtn").click();
+
+    cy.contains("Incorrect email or password");
+  });
+});
