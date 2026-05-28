ca4b046a37316d061c3c289d39593a81b04301a6
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-05-28 19:16:39 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    rules: {
+      eqeqeq: "error",
+      "no-unused-vars": "error",
+      "no-console": "warn",
+      semi: ["error", "always"],
+      "no-var": "error",
+      prefer-const: "error",
+    },
+  },
+];
