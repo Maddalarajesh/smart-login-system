61a6c916fea059b1d72b39c749ddf4f6a09f2318
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-05-29 15:24:54 +0530

    Create test-secrets.js

---
+++ test-secrets.js
@@ -0,0 +1,5 @@
+// This is a dummy test file to verify our DevOps TruffleHog pipeline setup
+const aws_access_key = "AKIAIOSFODNN7EXAMPLE";
+const aws_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+console.log("Testing secret detection...");
